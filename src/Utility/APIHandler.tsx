c4678495cca56ee44cb3615b941527fb49733c29
Reza Kelidari <re.kelidari@gmail.com>
2024-08-21 15:00:56 +0330

    Clearing git cache

--- src/Utility/APIHandler.tsx
+++ src/Utility/APIHandler.tsx
@@ -1,7 +1,10 @@
+const apiKey: string = "Bearer 205942f6e3a6272624f6d1f457d595dc";
+
 export enum Type {
   Upcoming = "upcoming",
   Popular = "popular",
 }
+
 export interface Movie {
   adult: boolean;
   backdrop_path: string;
@@ -18,6 +21,7 @@ export interface Movie {
   vote_average: number;
   vote_count: number;
 }
+
 interface Response {
   page: number;
   results: Array<Movie>;
@@ -30,7 +34,7 @@ export default async function getContent(content: Type): Promise<Response> {
     method: "GET",
     headers: {
       accept: "application/json",
-      Authorization: "Bearer 205942f6e3a6272624f6d1f457d595dc",
+      Authorization: apiKey,
     },
   })
     .then((response) => response.json())
